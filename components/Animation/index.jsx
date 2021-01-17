c1b126c13a47c68a4cc065d41b5b34c46636e5a0
Roger <roger.goma96@gmail.com>
2021-01-17 02:16:05 +0100

    Aligned text of animation

--- components/Animation/index.jsx
+++ components/Animation/index.jsx
@@ -1,63 +1,89 @@
+import { useEffect, useState } from "react";
 import { colors } from "../../styles/theme";
 
-const Animation = () => (
-  <>
-    <section>
-      <h1>
-        <span>Bihotz Studio</span>
-        <span>Bihotz Studio</span>
-        <span>Bihotz Studio</span>
-      </h1>
-    </section>
+const Animation = () => {
+  const [heightOfAnimation, setHeightOfAnimation] = useState("0px");
 
-    <style jsx>
-      {`
-        section {
-          background: ${colors.primary};
-          border-right: 1px solid ${colors.black};
-          height: 100%;
-          overflow: hidden;
-          position: fixed;
-          top: 0;
-          width: 20%;
-        }
+  const handleResize = () =>
+    setHeightOfAnimation(
+      `${document.getElementsByTagName("h1")[0].offsetWidth}px`
+    );
 
-        h1 {
-          font-weight: normal;
-          position: absolute;
-        }
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
 
-        span {
-          animation: move-to-top 12s linear infinite;
-          font-size: 130px;
-          left: 10%;
-          letter-spacing: -4px;
-          position: absolute;
-          top: -100vh;
-          transform: rotate(-90deg);
-          transform-origin: 0 0;
-          white-space: nowrap;
-        }
+    setHeightOfAnimation(
+      `${document.getElementsByTagName("h1")[0].offsetWidth}px`
+    );
 
-        span:nth-child(2) {
-          animation-delay: 4s;
-        }
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
-        span:nth-child(3) {
-          animation-delay: 8s;
-        }
+  return (
+    <>
+      <section>
+        <h1>
+          <span>Bihotz Studio</span>
+          <span>Bihotz Studio</span>
+          <span>Bihotz Studio</span>
+        </h1>
+      </section>
 
-        @keyframes move-to-top {
-          0% {
-            top: 250vh;
+      <style jsx>
+        {`
+          section {
+            background: ${colors.primary};
+            border-right: 1px solid ${colors.black};
+            height: 100%;
+            overflow: hidden;
+            position: fixed;
+            top: 0;
+            width: 20%;
           }
-          100% {
+
+          h1 {
+            font-weight: normal;
+            height: 100%;
+            margin: 0;
+            position: absolute;
+            width: 100%;
+          }
+
+          span {
+            align-items: center;
+            animation: move-to-top 12s linear infinite;
+            display: flex;
+            font-size: 130px;
+            letter-spacing: -4px;
+            position: absolute;
             top: -100vh;
+            transform: rotate(-90deg);
+            transform-origin: 0 0;
+            user-select: none;
+            white-space: nowrap;
+            height: ${heightOfAnimation};
           }
-        }
-      `}
-    </style>
-  </>
-);
+
+          span:nth-child(2) {
+            animation-delay: 4s;
+          }
+
+          span:nth-child(3) {
+            animation-delay: 8s;
+          }
+
+          @keyframes move-to-top {
+            0% {
+              top: 250vh;
+            }
+            100% {
+              top: -100vh;
+            }
+          }
+        `}
+      </style>
+    </>
+  );
+};
 
 export default Animation;
