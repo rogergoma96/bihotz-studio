ae138cfa7724c9ef560cc411a664a6ae4a392e3c
Roger <roger.goma96@gmail.com>
2021-02-26 15:08:25 +0100

    chore: changed instagram link

--- components/Links/Links.jsx
+++ components/Links/Links.jsx
@@ -5,7 +5,7 @@ import styles from "./Links.module.css";
 const Links = ({ labels }) => (
   <section className={styles.container}>
     <Link text="PORTFOLIO" href="https://www.behance.net/lauracantabrana" />
-    <Link text={labels.follow} href="https://www.instagram.com/bihotz_studio" />
+    <Link text={labels.follow} href="https://www.instagram.com/bihotz.studio" />
   </section>
 );
 
